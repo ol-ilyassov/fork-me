b8e56352de019466d35dd8738ce16db7f9c9e17f
Olzhas <ol.ilyassov@gmail.com>
2024-10-02 22:06:05 +0500

    edit stepik-courses.sh: remove double quotes

--- stepik-courses.sh
+++ stepik-courses.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-curl -s https://stepik.org:443/api/course-lists\?page\=1 | jq '."course-lists"[].title'
+curl -s https://stepik.org:443/api/course-lists\?page\=1 | jq -r '."course-lists"[].title'
